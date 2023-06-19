accc953ba17c92d62e152cef7cfe2eb60a503d59
Riann Silva <ooriannsilva@gmail.com>
2023-06-19 20:42:28 -0300

    Validating forms

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -1,11 +1,58 @@
 package co.tiagoaguiar.fitnesstracker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeigth: EditText
+    private lateinit var editHeigth: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        editWeigth = findViewById(R.id.edit_imc_weigth)
+        editHeigth = findViewById(R.id.edit_imc_heigth)
+
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+
+        btnSend.setOnClickListener {
+            if (!validate()) {
+                Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+        }
+    }
+
+    private fun validate(): Boolean {
+
+        //OP 01 -> usar if / else
+//        if (editWeigth.text.toString().isNotEmpty() &&
+//            editHeigth.text.toString().isNotEmpty() &&
+//            editWeigth.text.toString().startsWith("0") &&
+//            editHeigth.text.toString().startsWith("0")) {
+//            return true
+//        } else {
+//            return false
+//        }
+
+        //OP 02 -> usar return pra simular if / else
+//        if (editWeigth.text.toString().isNotEmpty() &&
+//            editHeigth.text.toString().isNotEmpty() &&
+//            editWeigth.text.toString().startsWith("0") &&
+//            editHeigth.text.toString().startsWith("0")) {
+//            return true
+//        }
+//        return false
+
+        //OP 03 -> retorna direto ou true ou false
+        return (editWeigth.text.toString().isNotEmpty() &&
+            editHeigth.text.toString().isNotEmpty() &&
+            !editWeigth.text.toString().startsWith("0") &&
+            !editHeigth.text.toString().startsWith("0"))
     }
 }
