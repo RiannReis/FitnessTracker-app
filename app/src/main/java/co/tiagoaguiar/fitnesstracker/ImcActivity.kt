2589ce028b547b40dbb6961e6348ce2da6ba0f9b
Riann Silva <ooriannsilva@gmail.com>
2023-06-20 16:07:52 -0300

    Managing keyboard

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -1,6 +1,8 @@
 package co.tiagoaguiar.fitnesstracker
 
+import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -59,6 +61,9 @@ class ImcActivity : AppCompatActivity() {
             }
                 .create()
                 .show()
+
+            val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            service.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
 
 
         }
