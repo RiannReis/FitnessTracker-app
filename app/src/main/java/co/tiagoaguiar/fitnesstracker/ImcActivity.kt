8443b7d908ec9813b99723254795a2cd11620da3
Riann Silva <ooriannsilva@gmail.com>
2023-06-19 22:26:51 -0300

    Business rules

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -1,9 +1,11 @@
 package co.tiagoaguiar.fitnesstracker
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 
 class ImcActivity : AppCompatActivity() {
@@ -25,10 +27,75 @@ class ImcActivity : AppCompatActivity() {
                 Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
+
+            val weigth = editWeigth.text.toString().toInt()
+            val heigth = editHeigth.text.toString().toInt()
+
+            val result = calculateImc(weigth, heigth)
+
+            val imcResponseId = imcResponse(result)
+            Toast.makeText(this, imcResponseId, Toast.LENGTH_SHORT).show()
         }
     }
 
+    @StringRes
+    private fun imcResponse(imc: Double): Int {
+        //OP 03 -> usar return direto no when
+        return when {
+            imc < 15.0 -> R.string.imc_severely_low_weight
+            imc < 16.0 -> R.string.imc_very_low_weight
+            imc < 18.0 -> R.string.imc_low_weight
+            imc < 25.0 -> R.string.normal
+            imc < 30.0 -> R.string.imc_high_weight
+            imc < 35.0 -> R.string.imc_so_high_weight
+            imc < 40.0 -> R.string.imc_severely_high_weight
+            else -> R.string.imc_extreme_weight
+        }
+
+        //OP 01 -> if / else
+//        if (imc < 15.0){
+//            return R.string.imc_severely_low_weight
+//        }else if (imc < 16.0){
+//            return R.string.imc_very_low_weight
+//        }else if (imc < 18.5){
+//            return  R.string.imc_low_weight
+//        }else if (imc < 25.0){
+//            return R.string.normal
+//        }else if (imc < 30.0){
+//            return R.string.imc_high_weight
+//        }else if (imc < 35.0){
+//            return R.string.imc_so_high_weight
+//        }else if (imc < 40.0) {
+//            return R.string.imc_severely_high_weight
+//        }else{
+//            return R.string.imc_extreme_weight
+//        }
+
+        //OP 02 -> when
+//        when{
+//            imc < 15.0 -> return R.string.imc_severely_low_weight
+//            imc < 16.0 -> return R.string.imc_very_low_weight
+//            imc < 18.0 -> return R.string.imc_low_weight
+//            imc < 25.0 -> return R.string.normal
+//            imc < 30.0 -> return R.string.imc_high_weight
+//            imc < 35.0 -> return R.string.imc_so_high_weight
+//            imc < 40.0 -> return R.string.imc_severely_high_weight
+//            else -> return R.string.imc_extreme_weight
+//        }
+
+
+    }
+
+    private fun calculateImc(weigth: Int, heigth: Int): Double {
+        return weigth / ( (heigth / 100.0) * (heigth / 100.0) )
+    }
+
     private fun validate(): Boolean {
+        //OP 03 -> retorna direto ou true ou false
+        return (editWeigth.text.toString().isNotEmpty() &&
+                editHeigth.text.toString().isNotEmpty() &&
+                !editWeigth.text.toString().startsWith("0") &&
+                !editHeigth.text.toString().startsWith("0"))
 
         //OP 01 -> usar if / else
 //        if (editWeigth.text.toString().isNotEmpty() &&
@@ -49,10 +116,5 @@ class ImcActivity : AppCompatActivity() {
 //        }
 //        return false
 
-        //OP 03 -> retorna direto ou true ou false
-        return (editWeigth.text.toString().isNotEmpty() &&
-            editHeigth.text.toString().isNotEmpty() &&
-            !editWeigth.text.toString().startsWith("0") &&
-            !editHeigth.text.toString().startsWith("0"))
     }
 }
